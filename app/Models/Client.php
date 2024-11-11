1fbe3d8d0d55e367d7b67e9363dff71aa7c890c7
odil <odilorg@gmail.com>
2024-11-11 19:33:09 +0500

    respourcess are et

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Client extends Model
 {
     use HasFactory;
+
+    public function patients(): HasMany
+    {
+        return $this->hasMany(Package::class);
+    }
+
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(Package::class);
+    }
 }
