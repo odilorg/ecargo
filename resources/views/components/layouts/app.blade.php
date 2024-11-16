e378e1518089ea9d943508c1a40f11d67a3b40fc
Odiljon <odilorg@gmail.com>
2024-11-16 13:25:21 +0500

    app cahnge

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     
+        <script src="https://cdn.tailwindcss.com"></script>
+
         <title>{{ $title ?? 'Ecargo' }}</title>
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')
