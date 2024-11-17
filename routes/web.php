da6e06573b3285de3d914b247192f1a49095a768
odil <odilorg@gmail.com>
2024-11-17 16:56:15 +0500

    front end done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Livewire\Auth\ForgotPasswordPage;
 use App\Livewire\HomePage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\CancelPage;
+use App\Livewire\SuccessPage;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +25,9 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', HomePage::class);
+Route::get('/login', LoginPage::class);
+Route::get('/register', RegisterPage::class);
+Route::get('/forgot', ForgotPasswordPage::class);
+Route::get('/reset', ResetPasswordPage::class);
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);
