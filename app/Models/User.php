825044ac7de33f9eae70035563429d78b79b33c0
odil <odilorg@gmail.com>
2024-11-18 22:12:41 +0500

    fixed admin only

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,22 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->email === 'odilorg@gmail.com';
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -22,6 +29,9 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+
+
 
     /**
      * The attributes that should be hidden for serialization.
